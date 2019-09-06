aaecb665a96cbbf5475b931d787ad852560d29f0
Rovian Vieceli <rovian.vieceli@cwi.com.br>
2019-09-06 12:57:51 -0300

    adicionando nodemon

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('Hello Word!');
+    response.send('Hello!');
 });
 
 app.listen(3001);
