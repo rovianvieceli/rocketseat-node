7e530e3d019e1138fafc47b440e2251f1f1c71b8
Rovian Vieceli <rovian.vieceli@cwi.com.br>
2019-09-24 12:36:24 -0300

    Server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 // Iniciando app
 const app = express();
 
-// Iniciando o DB
+// Iniciando o DB sem user e pass
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
 
 // Rota raiz
