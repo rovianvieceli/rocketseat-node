1b08132853cff18506a4660606f30695000ef92d
Rovian Vieceli <rovian.vieceli@cwi.com.br>
2019-10-01 13:18:01 -0300

    update, show, delete

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -7,8 +7,26 @@ module.exports = {
         return result.json(product);
     },
 
+    async show(request, result) {
+        const product = await Product.findById(request.params.id);
+        return result.json(product);
+    },
+
     async store(request, result) {
         const product = await Product.create(request.body);
         return result.json(product);
+    },
+
+    async update(request, result) {
+        const product = await Product.findByIdAndUpdate(request.params.id, request.body, {
+            new: true
+        });
+        return result.json(product);
+    },
+
+    async destroy(request, result) {
+        const product = await Product.findByIdAndRemove(request.params.id);
+        return result.send();
     }
+
 };
