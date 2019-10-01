0d5533b38cb3f00e9f19fdfc7e59c9e5cf54ef5a
Rovian Vieceli <rovian.vieceli@cwi.com.br>
2019-10-01 13:31:34 -0300

    Corrigindo deprecated uso de funcao do mongoose

--- server.js
+++ server.js
@@ -8,13 +8,19 @@ const app = express();
 // Permitir envio em Json na Request
 app.use(express.json());
 
-
 // Iniciando o DB sem user e pass
-mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi', {
+        'useNewUrlParser': true,
+        'useUnifiedTopology': true,
+        'useFindAndModify': false
+    });
 
+// habilitando as models
 requireDir('./src/models/');
 
 // Rotas - Qualquer requisição em /api
 app.use('/api', require('./src/route.js'));
 
+// start do servidor
 app.listen(3001);
